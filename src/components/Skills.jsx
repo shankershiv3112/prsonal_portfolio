8e9449060080caaba78309b3a4841cd1830d278d
shiv shankar <68695149+shankershiv3112@users.noreply.github.com>
2025-01-11 13:57:21 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,38 +2,51 @@ import React from "react"
 import {
   DiHtml5,
   DiCss3,
-  DiSass,
+ DiPython,
   DiBootstrap,
   DiJavascript1,
   DiReact,
   DiNodejsSmall,
   DiMongodb,
   DiGithubBadge,
+  DiMysql,
+  DiGoogleAnalytics,
+  DiGoogleCloudPlatform,
 } from "react-icons/di"
+
 import Reveal from "./Reveal"
 
 const skills = [
     {
-      category: 'Frontend',
-      technologies: [
-        { name: 'HTML', icon: <DiHtml5 className='text-orange-600' /> },
-        { name: 'CSS', icon: <DiCss3 className='text-blue-600' /> },
-        { name: 'Sass', icon: <DiSass className='text-pink-600' /> },
-        { name: 'Bootstrap', icon: <DiBootstrap className='text-purple-600' /> },
-        { name: 'JavaScript', icon: <DiJavascript1 className='text-yellow-500' /> },
-        { name: 'React', icon: <DiReact className='text-blue-500' /> },
-      ],
-    },
+        category: 'Fullstack',
+        technologies: [
+          { name: 'Node Js', icon: <DiNodejsSmall className='text-green-500' /> },
+          { name: 'MongoDB', icon: <DiMongodb className='text-red-600' /> },
+          { name: 'React', icon: <DiReact className='text-blue-500' /> },
+          { name: 'GitHub', icon: <DiGithubBadge className='text-gray-600' /> },
+          { name: 'JavaScript', icon: <DiJavascript1 className='text-yellow-500' /> },
+          { name: 'Bootstrap', icon: <DiBootstrap className='text-purple-600' /> },
+        
+        ],
+      },
+
+
+
+
     {
-      category: 'Fullstack',
+      category: 'Others',
       technologies: [
-        { name: 'Node Js', icon: <DiNodejsSmall className='text-green-500' /> },
-        { name: 'MongoDB', icon: <DiMongodb className='text-green-600' /> },
-        { name: 'React', icon: <DiReact className='text-blue-500' /> },
-        { name: 'GitHub', icon: <DiGithubBadge className='text-gray-600' /> },
+        
+        { name: 'MySql', icon: <DiMysql className='text-blue-600' /> },
+        { name: 'Python', icon: <DiPython className='text-yellow-600' /> },
+        { name: 'Google Analytics', icon: <DiGoogleAnalytics className='text-green-500' /> },
+        { name: 'Google Cloud Platform', icon: <DiGoogleCloudPlatform className='text-orange-500' /> },
+      
+        
       ],
     },
-  ]
+]
+    
 
 const Skills = () => {
   return (
@@ -41,7 +54,7 @@ const Skills = () => {
         <Reveal>
         <h2 className="text-3xl font-bold mb-4 text-center">Skills</h2>
         <p className="text-center mb-8">
-            I worked on various frontend and fullstack projects. Check them <a href="#" className="underline">there</a>.
+            I worked on various fullstack  and other projects. Check them <a href="#" className="underline">there</a>.
         </p>
 
         <div className="flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-8
